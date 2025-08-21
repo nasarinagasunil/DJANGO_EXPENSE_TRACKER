e9e1a792a42cf43c84f97bcf5bb561d2a5b2546e
nasarinagasunil <nagasunilnasari@gmail.com>
2025-08-21 11:12:08 +0530

    Model the DataBase

--- tracker/models.py
+++ tracker/models.py
@@ -2,3 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+class CurrentBalance(models.Model):
+    current_balance = models.FloatField(default=0.0)
+
+class TrackingHistory(models.Model):
+    current_balance = models.ForeignKey(CurrentBalance, on_delete=models.CASCADE)
+    expense_type = models.CharField(max_length=10, choices=[('DEBIT', 'Debit'), ('CREDIT', 'Credit')])
+    amount = models.FloatField()
+    description = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from tracker.models import CurrentBalance, TrackingHistory
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        description = request.POST.get('description')
+        amount = request.POST.get('amount')
+        expense_type = 'DEBIT' if float(amount) < 0 else 'CREDIT'
+        current_balance, _ = CurrentBalance.objects.get_or_create(id=1)
+        tracking_history = TrackingHistory.objects.create(
+            current_balance=current_balance,
+            expense_type=expense_type,
+            amount=amount,
+            description=description
+        )
+        current_balance.current_balance += float(tracking_history.amount)
+        current_balance.save()
+
+        return redirect("/")
+
+     
+    # Handle form submission here
     return render(request, "index.html")
